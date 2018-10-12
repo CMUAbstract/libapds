43136deacba74c724771b3b6c7bd0675f19fec3f
Graham Gobieski <graham.gobieski@gmail.com>
2018-10-11 21:49:45 -0400

    added raw gesture function

--- src/include/libapds/gesture.h
+++ src/include/libapds/gesture.h
@@ -16,6 +16,7 @@ void apds_disable(void);
 
 // Function for capturing gesture direction once apds is already initialized
 gest_dir apds_get_gesture(void);
+void apds_get_raw_gesture(gesture_data_t *gesture_data_, uint8_t *num_samps);
 
 // Board specific function to clear out the apds registers after running a gesture
 // Note: primarily necessary for testing under continuous power
